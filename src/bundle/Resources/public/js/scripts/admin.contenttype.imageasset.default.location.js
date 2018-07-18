d487a98f33e62bb6eeb219b029fdfac0c7bd82be
Dawid Parafiński <dawid.parafinski@ez.no>
2018-07-18 16:53:21 +0200

    EZP-29083: Validation and default location for selecting content

---
+++ src/bundle/Resources/public/js/scripts/admin.contenttype.imageasset.default.location.js
@@ -0,0 +1,40 @@
+(function (global, doc, React, ReactDOM) {
+    const btns = doc.querySelectorAll('.btn--udw-imageasset-default-location');
+    const udwContainer = doc.getElementById('react-udw');
+    const token = doc.querySelector('meta[name="CSRF-Token"]').content;
+    const siteaccess = doc.querySelector('meta[name="SiteAccess"]').content;
+    const closeUDW = () => ReactDOM.unmountComponentAtNode(udwContainer);
+    const onConfirm = (btn, items) => {
+        closeUDW();
+
+        const locationId = items[0].id;
+        const locationName = items[0].ContentInfo.Content.Name;
+        const objectRelationListSettingsWrapper = btn.closest('.ezimageasset-settings');
+        const objectRelationSettingsWrapper = btn.closest('.ezimageasset-settings');
+
+        if (objectRelationListSettingsWrapper) {
+            objectRelationListSettingsWrapper.querySelector(btn.dataset.relationRootInputSelector).value = locationId;
+            objectRelationListSettingsWrapper.querySelector(btn.dataset.relationSelectedRootNameSelector).innerHTML = locationName;
+        } else {
+            objectRelationSettingsWrapper.querySelector(btn.dataset.relationRootInputSelector).value = locationId;
+            objectRelationSettingsWrapper.querySelector(btn.dataset.relationSelectedRootNameSelector).innerHTML = locationName;
+        }
+    };
+    const onCancel = () => closeUDW();
+    const openUDW = (event) => {
+        event.preventDefault();
+
+        const config = JSON.parse(event.currentTarget.dataset.udwConfig);
+        ReactDOM.render(React.createElement(global.eZ.modules.UniversalDiscovery, Object.assign({
+            onConfirm: onConfirm.bind(null, event.currentTarget),
+            onCancel,
+            confirmLabel: 'Confirm location',
+            title: event.currentTarget.dataset.universaldiscoveryTitle,
+            multiple: false,
+            startingLocationId: window.eZ.adminUiConfig.universalDiscoveryWidget.startingLocationId,
+            restInfo: { token, siteaccess },
+        }, config)), udwContainer);
+    };
+
+    btns.forEach(btn => btn.addEventListener('click', openUDW, false));
+})(window, window.document, window.React, window.ReactDOM);
